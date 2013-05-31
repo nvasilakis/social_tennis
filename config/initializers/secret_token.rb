312d1cc6f8e4ba175829cd212589623b9a31beb0
Filippos Vasilakis <vasilakisfil@gmail.com>
2013-05-31 19:50:25 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialTennis::Application.config.secret_token = '31bc24b29d9a5bbd054617f147d2669d221ecbb60dd4941d6a9e4dfd91d32c19adb8f5f65ee0e4d2338f89f4f8fafd489bc330f1f51611a27d1823475d3fd4df'
