01c04cd237ae007d04d172094b91faf138334678
Filippos Vasilakis <vasilakisfil@gmail.com>
2013-06-08 21:20:16 +0300

    Refactor feature tests

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,26 +1,27 @@
 require 'spec_helper'
 
-  feature  "Home page" do
-    
-    scenario "should have the content 'Social Tennis'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Social Tennis')
-    end
+
+feature  "Home page" do
+  let(:base_title) { "Social Tennis"}
+
+  scenario "User visits Home page" do
+    visit '/static_pages/home'
+
+    expect(page).to have_title("#{base_title} | Home")
+    expect(page).to have_content('Social Tennis')
   end
-  
 
- feature "About page" do
-     
-   scenario "should have the content 'About'" do
-     visit '/static_pages/about'
-     expect(page).to have_content('About')
-   end
- end
+  scenario "has the content 'About'" do
+    visit '/static_pages/about'
 
- feature "Contact Us page" do
+    expect(page).to have_title("#{base_title} | About")
+    expect(page).to have_content("About")
+  end
 
-   scenario "should have the content 'Contact Us'" do
-     visit '/static_pages/contact'
-     expect(page).to have_content('Contact Us')
-   end
- end
+  scenario "has the content 'Contact Us'" do
+    visit '/static_pages/contact'
+
+    expect(page).to have_title("#{base_title} | Contact")
+    expect(page).to have_content("Contact Us")
+  end
+end
